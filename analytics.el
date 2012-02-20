e6d3c5f22ca5880a9e4daa8d2bb71d52bd9d8d75
mjsteger <mjsteger1@gmail.com>
2012-02-20 11:55:37 -0800

    Completely revamped the analytics framework, should now work as desired

--- analytics.el
+++ analytics.el
@@ -1,4 +1,4 @@
-(defgroup analytics nil
+ (defgroup analytics nil
 "The Analytics platform that Mikey is writing")
 
 (defvar *temp-alist nil)
@@ -13,61 +13,101 @@
   :type 'file
   :group 'analytics)
 
-(defvar *analytics-save-keystrokes-timer nil
-"Timer so that we send the keystrokes every so often")
+(defcustom keystroke-markov-state-file "~/.keystrokes-markov"
+  "File used to persist the keystrokes across sessions"
+  :type 'file
+  :group 'analytics)
 
-(defvar *functions-count #s(hash-table size 1000 test equal data ()))
-(defvar *keystrokes-count #s(hash-table size 2000 test equal data()))
+(defvar *analytics-save-keystrokes-timers '()
+  "Timer so that we send the keystrokes every so often")
+
+(defvar last-key nil)
+(defvar *functions-count '())
+(defvar *keystrokes-count '())
+(defvar *keystroke-markov-count '())
+
+(defun save-alist-keystrokes (file)
+  (when (not (equal *keystrokes-count nil))
+    (with-temp-buffer
+      (insert (format "%s" *keystrokes-count))
+      (write-region (point-min) (point-max) (eval file)))))
+
+(defun save-alist-functions (file)
+  (when (not (equal *functions-count nil))
+    (with-temp-buffer
+      (insert (format "%s" *functions-count))
+      (write-region (point-min) (point-max) (eval file)))))
+
+(defun save-alist-keystrokes-markov (file)
+  (when (not (equal *keystroke-markov-count nil))
+    (with-temp-buffer
+      (insert (format "%s" *keystroke-markov-count))
+      (write-region (point-min) (point-max) (eval file)))))
+
+(defun analytics-post-command-hook ()
+  (when (not (equal this-command 'self-insert-command))
+      (add-function this-command))
+  (setq this-command last-command-event)
+  (when (not (equal nil last-key))
+    (add-keystroke-markov this-command last-key))
+  (setq last-key this-command)
+  (add-keystroke last-key))
 
 
-(defun hash-to-alist (key value)
-  (setq *temp-alist (cons (cons key value) *temp-alist)))
+(defun analytics-add-hooks ()
+  (add-hook 'post-command-hook 'analytics-post-command-hook)
+  (add-hook 'kill-emacs-hook 'send-keystrokes))
 
-(defun hashToAlist (hash)
-  (setq *temp-alist nil)
-  (maphash 'hash-to-alist hash))
-
-(defun getKeystrokeForCommand (command valueToSet)
-  (with-temp-buffer
-    (where-is command t)
-    (set valueToSet (eval (buffer-string)))))
-
-
-(defun load-hash (file variable)
+(defun load-alist (file variable)
   (when (file-readable-p file)
     (with-temp-buffer
       (insert-file-contents-literally file)
-      (set variable (eval (buffer-string))))))
+      (set variable (read (buffer-string))))))
 
-(defun save-alist (file variable)
-  (when (not(equal variable nil))
-    (hashToAlist variable)
-    (with-temp-buffer
-      (insert (format "%s" *temp-alist))
-      (write-region (point-min) (point-max) (eval file)))))
+(defun add-keystroke (key-pressed)
+  (let ((key-cell (assoc key-pressed *keystrokes-count)))
+     (if (not (equal key-cell nil))
+     	(setcdr key-cell (+ 1 (cdr key-cell)))
+       (setq *keystrokes-count (cons (cons key-pressed 1) *keystrokes-count)))))
 
+(defun add-function (key-pressed)
+  (let ((key-cell (assoc key-pressed *functions-count)))
+     (if (not (equal key-cell nil))
+     	(setcdr key-cell (+ 1 (cdr key-cell)))
+       (setq *functions-count (cons (cons key-pressed 1) *functions-count)))))
 
-(defun add-keystroke (variable command)
-  (puthash command (+ 1 (gethash command variable 0)) variable))
+(defun add-keystroke-markov (key-pressed last-key)
+  (let ((key-cell (assoc last-key (assoc key-pressed *keystroke-markov-count)))
+	(key-cell-general (assoc key-pressed *keystroke-markov-count)))
+    (if (equal key-cell-general nil)
+	(setq *keystroke-markov-count (cons (list key-pressed (list last-key 1)) *keystroke-markov-count))
+      (if (equal key-cell nil)
+	  (setcdr key-cell-general (list (cadr key-cell-general) (list last-key 1)))
+	(setcdr key-cell (list(+ 1 (cadr key-cell))))
+	))))
 
-;;(add-hook 'post-self-insert-hook 'analytics-post-insert-hook)
-
-(defun analytics-post-command-hook ()
-  (if (not(equal this-command 'self-insert-command))
-      (add-keystroke *functions-count this-command)
-    (add-keystroke *keystrokes-count last-command-event)))
-
-(defun analytics-add-hooks ()
-  (add-hook 'post-command-hook 'analytics-post-command-hook))
-;; (defun analytics-post-insert-hook ()
-;;   (add-keystroke *keystrokes-count last-command-event))
-
-(save-alist keystroke-state-file *keystrokes-count)
 (defun send-keystrokes ()
-  (run-with-timer 1 nil 'save-alist function-state-file *functions-count)
-  (run-with-timer 1 nil 'save-alist keystroke-state-file *keystrokes-count))
+  (setq *analytics-save-keystrokes-timers (cons (list (run-with-idle-timer 10 t 'save-alist-functions function-state-file)) *analytics-save-keystrokes-timers))
+  (setq *analytics-save-keystrokes-timers (cons (list (run-with-idle-timer 10 t 'save-alist-keystrokes keystroke-state-file)) *analytics-save-keystrokes-timers ))
+  (setq *analytics-save-keystrokes-timers (cons (list (run-with-idle-timer 10 t 'save-alist-keystrokes-markov keystroke-markov-state-file)) *analytics-save-keystrokes-timers)))
 
-(provide 'analytics)
-(analytics-add-hooks)
-(send-keystrokes)
+(defun stop-analytics-timers ()
+  (when (not (equal nil *analytics-save-keystrokes-timers))
+    (mapcar (lambda (x) (cancel-timer (car x))) *analytics-save-keystrokes-timers)
+    (setq *analytics-save-keystrokes-timers nil)))
 
+(defun load-trackers-if-nil ()
+  (when (equal nil *functions-count)
+    (load-alist function-state-file '*functions-count))
+  (when (equal nil *keystrokes-count)
+    (load-alist keystroke-state-file '*keystrokes-count))
+  (when (equal nil *keystroke-markov-count)
+    (load-alist keystroke-markov-state-file '*keystroke-markov-count))
+  )
+
+(defun enable-analytics ()
+  (analytics-add-hooks)
+  (load-trackers-if-nil)
+  (stop-analytics-timers)
+  (send-keystrokes))
+(enable-analytics)
